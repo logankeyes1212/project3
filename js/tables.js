6b5c57eb7f3bb9109ada53b0fadfe2c7d1b01ea5
Logan <logankeyes1212@gmail.com>
2019-04-02 19:13:13 -0500

    everything is on the test page for container that holds leaders sleepers and treading NOT WORKING IN INDEX.html

---
+++ js/tables.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+    // leaderPG()
+// const dropDown = 
+// $('.dropdown-trigger').dropdown();
+//     switch(dropDown){
+//         case leaderPG():
+//         break;
+//         case leaderSF():
+//         break;
+//         case leaderSG():
+//         break;
+//         case leaderPF():
+//         break;
+//         case leaderC():
+//         break;
+
+// }
+
+
+    // function leaderPG() {
+        $("#leadersPG").append(
+            "<tr>" +
+            "<td>" + "<p>"+ "twoPoints.val()" + "</p>"  + "</td>" +
+            "<td>" + "threePoints.val()" + "</td>" +
+            "<td>" + blocks.val()      + "</td>" +
+            "<td>" + assist.val()      + "</td>" +
+            "<td>" + rebounds.val()    + "</td>" +
+            "<td>" + steals.val()      + "</td>" +
+            "</tr>"
+        )
+    // }
+    function leaderSF() {
+        $("#leadersSF").append(
+            "<tr>" +
+            "<td>" + twoPoints.val()   + "</td>" +
+            "<td>" + threePoints.val() + "</td>" +
+            "<td>" + blocks.val()      + "</td>" +
+            "<td>" + assist.val()      + "</td>" +
+            "<td>" + rebounds.val()    + "</td>" +
+            "<td>" + steals.val()      + "</td>" +
+            "</tr>"
+        )
+    }
+    function leaderSG() {
+        $("#leadersSG").append(
+            "<tr>" +
+            "<td>" + twoPoints.val() + "</td>" +
+            "<td>" + threePoints.val() + "</td>" +
+            "<td>" + blocks.val() + "</td>" +
+            "<td>" + assist.val() + "</td>" +
+            "<td>" + rebounds.val() + "</td>" +
+            "<td>" + steals.val() + "</td>" +
+            "</tr>"
+        )
+    }
+    function leaderPF() {
+        $("#leadersPF").append(
+            "<tr>" +
+            "<td>" + twoPoints.val() + "</td>" +
+            "<td>" + threePoints.val() + "</td>" +
+            "<td>" + blocks.val() + "</td>" +
+            "<td>" + assist.val() + "</td>" +
+            "<td>" + rebounds.val() + "</td>" +
+            "<td>" + steals.val() + "</td>" +
+            "</tr>"
+        )
+    }
+    
+    function leaderC() {
+        $("#leadersC").append(
+            "<tr>" +
+            "<td>" + twoPoints.val() + "</td>" +
+            "<td>" + threePoints.val() + "</td>" +
+            "<td>" + blocks.val() + "</td>" +
+            "<td>" + assist.val() + "</td>" +
+            "<td>" + rebounds.val() + "</td>" +
+            "<td>" + steals.val() + "</td>" +
+            "</tr>"
+        )
+    }
+})
