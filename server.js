b1e9997bf03eae3a501e3bd03793b040aaa81eb2
Logan <logankeyes1212@gmail.com>
2019-04-08 10:46:33 -0500

    readme and testing heroku links

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ app.use(passport.session()) // calls the deserializeUser
 app.use('/user', user)
 
 // Starting Server 
-app.listen(PORT, () => {
-	console.log(`App listening on PORT: ${PORT}`)
-})
+app.listen(process.env.PORT || 8080, function(){
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+// app.listen(PORT, () => {
+// 	console.log(`App listening on PORT: ${PORT}`)
+// })
