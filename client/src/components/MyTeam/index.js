527cfcdd14d347e168e0eea6d8219fcac52ff78d
Logan <logankeyes1212@gmail.com>
2019-04-07 22:34:57 -0500

    table and searh done

--- client/src/components/MyTeam/index.js
+++ client/src/components/MyTeam/index.js
@@ -6,25 +6,63 @@ import '../../assets/css/style.css'
 
 class MyTeam extends Component {
     state = {
-        default: "my team",
+        default: ""
+        // <h4>Checkout my team or search for new players</h4>,
 
 
     }
     constructor() {
         super()
     }
+    componentDidMount(){
+        this.myTeamDiv();
+    }
     myTeamDiv = data => {
-        this.setState({ default: "My Team================" });
+        this.setState({
+            default: 
+            <div>
+            <h4>My Team</h4>
+            < table class="responsive-table striped highlight" >
+                <thead>
+                    <tr>
+                        <th>Position</th>
+                        <th>Name</th>
+                        <th>Team</th>
+                    </tr>
+                </thead>
 
+                <tbody>
+                    <tr>
+                        <td>SG</td>
+                        <td>LeBron James</td>
+                        <td>L.A. Lakes</td>
+                    </tr>
+                    <tr>
+                        <td>C</td>
+                        <td>Karl Anthony Towns</td>
+                        <td>MN Timberwolves</td>
+                    </tr>
+                    <tr>
+                        <td>SG</td>
+                        <td>Kevin Durant</td>
+                        <td>SF Golden State Warriors</td>
+                    </tr>
+                </tbody>
+            </table >
+</div>
+
+        });
     }
     searchDiv = data => {
-        this.setState({ default: <form>
-            <label>
-                Player Name:
+        this.setState({
+            default: <form>
+                <label>
+                    Player Name:
                 <input type="text" name="name" />
-            </label>
-            <input type="submit" value="Submit" />
-        </form> });
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        });
 
     }
 
@@ -40,7 +78,7 @@ class MyTeam extends Component {
                         <div className="col s12 center">
                             <a onClick={this.myTeamDiv} className="btn-large waves-effect waves-light teal lighten-1">My Team</a>
                             <a onClick={this.searchDiv} className="btn-large waves-effect waves-light teal lighten-1">Search</a>
-                            
+
                             <div>{this.state.default}</div>
 
                         </div>
